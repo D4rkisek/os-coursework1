1e14d62dc17abbb65ca4d1bc7a3dec40565c7fa1
D4rkisek <maruf.f@hotmail.com>
2023-02-18 22:41:15 +0000

    .

--- src/main/java/RRScheduler.java
+++ src/main/java/RRScheduler.java
@@ -1,3 +1,5 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Properties;
 
 /**
@@ -7,28 +9,66 @@ import java.util.Properties;
  */
 public class RRScheduler extends AbstractScheduler {
 
-  // TODO
+  private int timeQuantum;
+  private Queue<Process> readyQueue; //ready/unblock queue
 
-  /**
-   * Adds a process to the ready queue.
-   * usedFullTimeQuantum is true if process is being moved to ready
-   * after having fully used its time quantum.
-   */
-  public void ready(Process process, boolean usedFullTimeQuantum) {
+    /**
+     * Initializes the scheduler from the given parameters
+     */
+    @Override
+    public void initialize(Properties parameters) {
+        this.readyQueue = new LinkedList<>();
+        this.timeQuantum = Integer.parseInt(parameters.getProperty("timeQuantum"));
+    }
 
-    // TODO
+    /**
+     * Adds a process to the ready queue.
+     * usedFullTimeQuantum is true if process is being moved to ready
+     * after having fully used its time quantum.
+     */
+    @Override
+    public void ready(Process process, boolean usedFullTimeQuantum) {
+        if (usedFullTimeQuantum) {
+            // If the process has fully used its time quantum, add it to the back of the queue
+            this.readyQueue.add(process);
+        } else {
+            // If the process has not fully used its time quantum, add it to the front of the queue
+            this.readyQueue.offer(process);
+        }
+    }
 
-  }
+    /**
+     * Removes the next process to be run from the ready queue
+     * and returns it.
+     * Returns null if there is no process to run.
+     */
+    @Override
+    public Process schedule() {
+        if (!this.readyQueue.isEmpty()) {
+            // Get the next process to run from the front of the queue
+            Process nextProcess = this.readyQueue.poll();
 
-  /**
-   * Removes the next process to be run from the ready queue 
-   * and returns it. 
-   * Returns null if there is no process to run.
-   */
-  public Process schedule() {
+            // Set the process state to RUNNING
+            nextProcess.state = Process.State.RUNNING;
 
-    // TODO
+            // Return the process
+            return nextProcess;
+        } else {
+            return null;
+        }
+    }
+    /**
+     * Returns the time quantum of this scheduler.
+     */
+    public int getTimeQuantum() {
+        return this.timeQuantum;
+    }
 
-    return null;
-  }
+    /**
+
+     Returns whether the scheduler is preemptive
+     */
+    public boolean isPreemptive() {
+        return false;
+    }
 }
