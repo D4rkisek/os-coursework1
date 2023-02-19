01f8abb3bc049d0c8c6be60578411456fb8e5775
D4rkisek <maruf.f@hotmail.com>
2023-02-19 23:59:03 +0000

    .

--- src/main/java/IdealSJFScheduler.java
+++ src/main/java/IdealSJFScheduler.java
@@ -1,34 +1,72 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Properties;
 
 /**
  * Ideal Shortest Job First Scheduler
- * 
+ *
  * @version 2017
  */
 public class IdealSJFScheduler extends AbstractScheduler {
 
-  // TODO
+  private Queue<Process> readyQueue;
 
+  /**
+   * Creates an instance of the Ideal SJF scheduler
+   */
+
+  @Override
+  public void initialize(Properties parameters) {
+    readyQueue = new LinkedList<>();
+  }
   /**
    * Adds a process to the ready queue.
    * usedFullTimeQuantum is true if process is being moved to ready
    * after having fully used its time quantum.
    */
   public void ready(Process process, boolean usedFullTimeQuantum) {
-
-    // TODO
-
+    if (usedFullTimeQuantum) {
+      // If the processor has fully used its time quantum, add it to the back of the queue
+      readyQueue.add(process);
+    } else {
+      // If the processor has not fully used its time quantum, add it to the front of the queue
+      readyQueue.offer(process);
+    }
   }
 
   /**
-   * Removes the next process to be run from the ready queue 
-   * and returns it. 
+   * Removes the next process to be run from the ready queue
+   * and returns it.
    * Returns null if there is no process to run.
    */
   public Process schedule() {
+    if (!readyQueue.isEmpty()) {
+      // Find the process with the shortest next burst time
+      Process shortestProcess = null;
+      int shortestBurst = Integer.MAX_VALUE;
+      for (Process p : readyQueue) {
+        int nextBurst = p.getNextBurst();
+        if (nextBurst < shortestBurst) {
+          shortestProcess = p;
+          shortestBurst = nextBurst;
+        }
+      }
 
-    // TODO
-
-    return null;
+      // Remove the process with the shortest next burst time from the queue and return it
+      readyQueue.remove(shortestProcess);
+      return shortestProcess;
+    } else {
+      return null;
+    }
   }
+
+
+  /**
+
+   Returns whether the scheduler is preemptive
+   */
+  public boolean isPreemptive() {
+    return false;
+  }
+
 }
