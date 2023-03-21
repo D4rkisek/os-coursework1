f1baf9d26a6d0ae6e98051f72d8875171dd643f4
D4rkisek <106534376+D4rkisek@users.noreply.github.com>
2023-03-21 21:50:21 +0000

    .

--- run.bat
+++ run.bat
@@ -2,6 +2,9 @@
 echo Running Experiment 1...
 
 echo experiment 1 seed 1
+REM run the input generator for seed1 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment1/seed1/input_parameters.prp experiment1/seed1/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed1/simulator_parameters_FirstComeFirstServe.prp experiment1/seed1/output_FirstComeFirstServe.out experiment1/seed1/inputs.in
 
@@ -19,6 +22,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed1/simu
 
 
 echo experiment 1 seed 2
+REM run the input generator for seed2 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment1/seed2/input_parameters.prp experiment1/seed2/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed2/simulator_parameters_FirstComeFirstServe.prp experiment1/seed2/output_FirstComeFirstServe.out experiment1/seed2/inputs.in
 
@@ -36,6 +42,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed2/simu
 
 
 echo experiment 1 seed 3
+REM run the input generator for seed3 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment1/seed3/input_parameters.prp experiment1/seed3/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed3/simulator_parameters_FirstComeFirstServe.prp experiment1/seed3/output_FirstComeFirstServe.out experiment1/seed3/inputs.in
 
@@ -53,6 +62,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed3/simu
 
 
 echo experiment 1 seed 4
+REM run the input generator for seed4 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment1/seed4/input_parameters.prp experiment1/seed4/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed4/simulator_parameters_FirstComeFirstServe.prp experiment1/seed4/output_FirstComeFirstServe.out experiment1/seed4/inputs.in
 
@@ -70,6 +82,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed4/simu
 
 
 echo experiment 1 seed 5
+REM run the input generator for seed5 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment1/seed5/input_parameters.prp experiment1/seed5/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/seed5/simulator_parameters_FirstComeFirstServe.prp experiment1/seed5/output_FirstComeFirstServe.out experiment1/seed5/inputs.in
 
@@ -90,6 +105,9 @@ echo Running Experiment 2...
 
 
 echo experiment 2 seed 1
+REM run the input generator for seed1 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment2/seed1/input_parameters.prp experiment2/seed1/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed1/simulator_parameters_FirstComeFirstServe.prp experiment2/seed1/output_FirstComeFirstServe.out experiment2/seed1/inputs.in
 
@@ -107,6 +125,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed1/simu
 
 
 echo experiment 1 seed 2
+REM run the input generator for seed2 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment2/seed2/input_parameters.prp experiment2/seed2/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed2/simulator_parameters_FirstComeFirstServe.prp experiment2/seed2/output_FirstComeFirstServe.out experiment2/seed2/inputs.in
 
@@ -124,6 +145,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed2/simu
 
 
 echo experiment 2 seed 3
+REM run the input generator for seed3 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment2/seed3/input_parameters.prp experiment2/seed3/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed3/simulator_parameters_FirstComeFirstServe.prp experiment2/seed3/output_FirstComeFirstServe.out experiment2/seed3/inputs.in
 
@@ -141,6 +165,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed3/simu
 
 
 echo experiment 2 seed 4
+REM run the input generator for seed4 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment2/seed4/input_parameters.prp experiment2/seed4/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed4/simulator_parameters_FirstComeFirstServe.prp experiment2/seed4/output_FirstComeFirstServe.out experiment2/seed4/inputs.in
 
@@ -158,6 +185,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed4/simu
 
 
 echo experiment 2 seed 5
+REM run the input generator for seed5 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment2/seed5/input_parameters.prp experiment2/seed5/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/seed5/simulator_parameters_FirstComeFirstServe.prp experiment2/seed5/output_FirstComeFirstServe.out experiment1/seed5/inputs.in
 
@@ -179,6 +209,9 @@ echo Running Experiment 3...
 
 
 echo experiment 3 seed 1
+REM run the input generator for seed1 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment3/seed1/input_parameters.prp experiment3/seed1/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed1/simulator_parameters_FirstComeFirstServe.prp experiment3/seed1/output_FirstComeFirstServe.out experiment3/seed1/inputs.in
 
@@ -196,6 +229,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed1/simu
 
 
 echo experiment 3 seed 2
+REM run the input generator for seed2 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment3/seed2/input_parameters.prp experiment3/seed2/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed2/simulator_parameters_FirstComeFirstServe.prp experiment3/seed2/output_FirstComeFirstServe.out experiment3/seed2/inputs.in
 
@@ -213,6 +249,10 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed2/simu
 
 
 echo experiment 3 seed 3
+REM run the input generator for seed3 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment3/seed3/input_parameters.prp experiment3/seed3/inputs.in
+
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed3/simulator_parameters_FirstComeFirstServe.prp experiment3/seed3/output_FirstComeFirstServe.out experiment3/seed3/inputs.in
 
@@ -230,6 +270,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed3/simu
 
 
 echo experiment 3 seed 4
+REM run the input generator for seed4 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment3/seed4/input_parameters.prp experiment3/seed4/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed4/simulator_parameters_FirstComeFirstServe.prp experiment3/seed4/output_FirstComeFirstServe.out experiment3/seed4/inputs.in
 
@@ -247,6 +290,9 @@ java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed4/simu
 
 
 echo experiment 3 seed 5
+REM run the input generator for seed5 folder
+java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment3/seed5/input_parameters.prp experiment3/seed5/inputs.in
+
 REM run the simulator for FirstComeFirstServe
 java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment3/seed5/simulator_parameters_FirstComeFirstServe.prp experiment3/seed5/output_FirstComeFirstServe.out experiment3/seed5/inputs.in
 
